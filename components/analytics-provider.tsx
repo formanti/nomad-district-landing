734f38f2bfe9e10894eade700a19437bc0f59c24
Jaime Garcia <jaimegarciaare@gmail.com>
2025-12-13 14:39:19 -0600

    feat: Add Meta Pixel tracking with ID 1714660455885941

--- components/analytics-provider.tsx
+++ components/analytics-provider.tsx
@@ -3,6 +3,7 @@
 import Script from "next/script";
 
 const GA_MEASUREMENT_ID = "G-32G6EVXGS1";
+const META_PIXEL_ID = "1714660455885941";
 
 export function AnalyticsProvider() {
     return (
@@ -20,6 +21,38 @@ export function AnalyticsProvider() {
           gtag('config', '${GA_MEASUREMENT_ID}');
         `}
             </Script>
+
+            {/* Meta Pixel */}
+            <Script id="meta-pixel" strategy="afterInteractive">
+                {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${META_PIXEL_ID}');
+          fbq('track', 'PageView');
+        `}
+            </Script>
+            <noscript>
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: 'none' }}
+                    src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+                    alt=""
+                />
+            </noscript>
         </>
     );
+}
+
+// Helper function to track button clicks
+export function trackInitiateCheckout() {
+    if (typeof window !== 'undefined' && (window as any).fbq) {
+        (window as any).fbq('track', 'InitiateCheckout');
+    }
 }
